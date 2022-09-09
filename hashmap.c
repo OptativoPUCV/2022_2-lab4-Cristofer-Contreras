d84b1d8c88388b384e7e30a453ffe93f0ecdbd02
Replit user <>
2022-09-09 16:48:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -10,50 +10,57 @@ typedef struct HashMap HashMap;
 int enlarge_called=0;
 
 struct HashMap {
-    Pair ** buckets;
-    long size; //cantidad de datos/pairs en la tabla
-    long capacity; //capacidad de la tabla
-    long current; //indice del ultimo dato accedido
+  Pair ** buckets;
+  long size; //cantidad de datos/pairs en la tabla
+  long capacity; //capacidad de la tabla
+  long current; //indice del ultimo dato accedido
 };
 
 Pair * createPair( char * key,  void * value) {
-    Pair * new = (Pair *)malloc(sizeof(Pair));
-    new->key = key;
-    new->value = value;
-    return new;
+  Pair * new = (Pair *)malloc(sizeof(Pair));
+  new->key = key;
+  new->value = value;
+  return new;
 }
 
 long hash( char * key, long capacity) {
-    unsigned long hash = 0;
-     char * ptr;
-    for (ptr = key; *ptr != '\0'; ptr++) {
-        hash += hash*32 + tolower(*ptr);
-    }
-    return hash%capacity;
+  unsigned long hash = 0;
+  char * ptr;
+  for (ptr = key; *ptr != '\0'; ptr++) {
+    hash += hash*32 + tolower(*ptr);
+  }
+  return hash%capacity;
 }
 
 int is_equal(void* key1, void* key2){
-    if(key1==NULL || key2==NULL) return 0;
-    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
-    return 0;
+  if(key1==NULL || key2==NULL) return 0;
+  if(strcmp((char*)key1,(char*)key2) == 0) return 1;
+  return 0;
 }
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-
+  
 
 }
 
 void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
+  enlarge_called = 1; //no borrar (testing purposes)
 
 
 }
 
 
 HashMap * createMap(long capacity) {
+  HashMap * map = (HashMap *)malloc(sizeof(HashMap));
 
-    return NULL;
+  if(map == NULL) return NULL;
+  map -> buckets = (Pair **)calloc(capacity, sizeof(Pair*));
+
+  map ->size =0;
+  map -> capacity = capacity;
+  map -> current = -1;
+  return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
@@ -64,15 +71,15 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) {   
 
 
-    return NULL;
+  return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
 
-    return NULL;
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
 
-    return NULL;
+  return NULL;
 }
